05d94379ac6d8ddc0a218cd5c9d0a10ff17f10f9
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-05 20:23:49 +0530

    React.memo, state down method

--- exp2/src/App.jsx
+++ exp2/src/App.jsx
@@ -1,12 +1,30 @@
-function App() {
-  
+import { useState } from "react";
+
+
+function App() { 
+  return(
+    <div>
+      <UpdateApp />
+      <Header title="Iqra"/><br />
+      <Header title="Nawab"/><br />
+      <Header title="Ayan"/><br />
+      <Header title="Aliza"/><br />
+    </div>
+  )
+}
+// Push the state down method, can use React.memo for re-render
+function UpdateApp(){
+
+  const [title, setTitle] = useState("Shariq");
+  function updateTitle(){
+    setTitle(`My name is ${Math.random()}`);
+  }
 
   return (
     <>
       <div>
-        <button>Click me to chNGE THE TITLE</button>
-      <Header title="Shariq"/>
-      <Header title="Iqra"/>
+        <button onClick={updateTitle}>Click me to change the title</button><br />
+      <Header title={title}/><br />
       </div>
     </>
   )
