8935106987cf421818b2d7863907e8d711c7698b
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-09 14:16:22 +0530

    learnt how to change dynamically

--- useEffectAssign2/src/App.jsx
+++ useEffectAssign2/src/App.jsx
@@ -4,11 +4,26 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-
+  const [selectedId, setSelectedId] = useState(1);
   return (
     <>
       <div>
-        <Todo id={1}/>
+        <button onClick={function(){
+          setSelectedId(1)
+        }}>1</button>
+        <button onClick={function(){
+          setSelectedId(2)
+        }}>2</button>
+
+        <button onClick={function(){
+          setSelectedId(3)
+        }}>3</button>
+
+        <button onClick={function(){
+          setSelectedId(4)
+        }}>4</button>
+
+        <Todo id={selectedId}/>
       </div>
     </>
   )
@@ -21,11 +36,14 @@ function Todo({id}){
     axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then(function(response){
       setTodo(response.data.todo)
     })
-   }, [])
+   }, [id])
 
 
    return (
     <>
+    <div>
+      Id: {id}
+    </div>
       <div>
         <h3>
           {todo.title}
