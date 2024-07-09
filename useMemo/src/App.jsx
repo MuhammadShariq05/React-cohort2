ded25b38fd4cca65d4390829c49628bf727dc2bc
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-09 16:50:12 +0530

    initial logic + e.target.value

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-  return (
-    <>
-     <div>
-      
-     </div>
-    </>
-  )
+
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
+
+  //e.target.value gets the value of the input from the dom
+  return <div>
+    <input onChange={function(e) {
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    <div className="name">Sum from 1 to {inputValue} is {count}</div>
+    <br />
+    <button onClick={() => {
+      setCounter(counter + 1);
+    }}>Counter {counter}</button>
+  </div>
 }
 
-export default App
+export default App;
