a98972fc3a55c8befe17f68cd0c32d95f395ded2
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-09 13:42:04 +0530

    initial commit

--- useEffectAssign2/src/App.jsx
+++ useEffectAssign2/src/App.jsx
@@ -1,15 +1,42 @@
 import { useState } from 'react'
-import { useEffect } from 'react'
-import axios from "axios"
 import './App.css'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
   return (
     <>
-      
+      <div>
+        <Todo id={1}/>
+      </div>
     </>
   )
+}
+
+function Todo({id}){
+  const [todo, setTodo] = useState([]);
+  // initial logic
+   useEffect(()=>{
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`).then(function(response){
+      setTodo(response.data.todo)
+    })
+   }, [])
+
+
+   return (
+    <>
+      <div>
+        <h3>
+          {todo.title}
+        </h3>
+        <h5>
+          {todo.description}
+        </h5>
+      </div>
+    </>
+   )
+
 }
 
 export default App
