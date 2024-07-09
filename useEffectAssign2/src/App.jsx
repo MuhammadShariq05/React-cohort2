107304aaede5394598df321170950c56f0b22579
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-09 13:40:40 +0530

    assignment 2 on useeffect

---
+++ useEffectAssign2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from "axios"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
