f1ac134ce6ad3c1b55a78ab5b64315c0d2637738
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-09 13:07:19 +0530

    fetching data from the backend and rendering it

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
-function App(){
-  const [count, setCount] = useState(0)
+function App() {
+  const [todos, setTodo] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos").then(function (response) {
+      setTodo(response.data.todos);
+    });
+  }, []);
+
+  return (
+    <>
+      {todos.map(todo => <Todo id={todo.id} title={todo.title} description={todo.description}/>)}
+    </>
+  );
+}
+
+function Todo({ title, description }) {
+  
 
   return (
     <>
       <div>
-        <button onClick={function(){
-          setCount(count + 1);
-        }}>click me</button>
+        <h3>{title}</h3>
+        <h5>{description}</h5>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
