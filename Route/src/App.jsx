d8e010616ced417162ac9df154abf07a552849d3
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-13 20:50:51 +0530

    learnt useNavigate

--- Route/src/App.jsx
+++ Route/src/App.jsx
@@ -1,39 +1,47 @@
 import { useState } from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
 import './App.css';
 import { Dashboard } from './components/Dashboard';
 import { Landing } from './components/Landing';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <div>
       {/* client site routing */}
-      <div>
+      <BrowserRouter>
+        <Approute />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/landing" element={<Landing />}/>
+        </Routes>
+      </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+function Approute(){
+  const navigate = useNavigate();
+
+  return(
+    <div>
         <div className='topbar'>
           Top Bar
           <button onClick={() => {
             // 1st method global locatoin object in react 
-            window.location.href="/landing";
+            navigate("/landing");
           }}>
             landing
           </button>
           <button onClick={() => {
-            window.location.href="/dashboard";
+            navigate("/dashboard");
           }}> 
             dashboard
           </button>
         </div>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />}/>
-            <Route path="/landing" element={<Landing />}/>
-          </Routes>
-      </BrowserRouter>
-      </div>
-    </>
+  </div>
   )
 }
 
