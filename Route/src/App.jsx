c9fdbb00c649752d087d600fb7a193843725ee1c
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-13 21:05:24 +0530

    introducing React.lazy

--- Route/src/App.jsx
+++ Route/src/App.jsx
@@ -1,48 +1,56 @@
-import { useState } from 'react';
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
-import './App.css';
-import { Dashboard } from './components/Dashboard';
-import { Landing } from './components/Landing';
-
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+// import { Dashboard } from './components/Dashboard';
+// import { Landing } from './components/Landing';
+const Dashboard = React.lazy(() => {
+  import("./components/Dashboard");
+});
+const Landing = React.lazy(() => {
+  import("./components/Landing");
+});
 function App() {
   return (
     <>
-    <div>
-      {/* client site routing */}
-      <BrowserRouter>
-        <Approute />
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/landing" element={<Landing />}/>
-        </Routes>
-      </BrowserRouter>
+      <div>
+        {/* client site routing */}
+        <BrowserRouter>
+          <Approute />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/landing" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-function Approute(){
+function Approute() {
   const navigate = useNavigate();
 
-  return(
+  return (
     <div>
-        <div className='topbar'>
-          Top Bar
-          <button onClick={() => {
-            // 1st method global locatoin object in react 
+      <div className="topbar">
+        Top Bar
+        <button
+          onClick={() => {
+            // 1st method global locatoin object in react
             navigate("/landing");
-          }}>
-            landing
-          </button>
-          <button onClick={() => {
+          }}
+        >
+          landing
+        </button>
+        <button
+          onClick={() => {
             navigate("/dashboard");
-          }}> 
-            dashboard
-          </button>
-        </div>
-  </div>
-  )
+          }}
+        >
+          dashboard
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Route/src/components/Dashboard.jsx
+++ Route/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-export function Dashboard(){
+export default function Dashboard(){
   return(
     <div style={{alignItems: "center", display: "flex"}}>
       Dashboard
