27ea516d37ac90ae6a3b22b91ef670c162830db3
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-15 10:40:56 +0530

    learnt how to fetch data in atom

--- Recoil/7.4-async-queries/src/App.jsx
+++ Recoil/7.4-async-queries/src/App.jsx
@@ -15,22 +15,16 @@ function MainApp() {
   const [networkCount, setNetworkCount] = useRecoilState(notifications)
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
-  useEffect(() => {
-    // fetch
-    axios.get("https://sum-server.100xdevs.com/notifications")
-      .then(res => {
-        setNetworkCount(res.data)
-      })
-  }, [])
+  
 
   return (
     <>
       <button>Home</button>
       
-      <button>My network ({networkCount.networks >= 100 ? "99+" : networkCount.networks})</button>
+      <button>My network {networkCount.networks >= 100 ? "99+" : networkCount.networks}</button>
       <button>Jobs {networkCount.jobs}</button>
-      <button>Messaging ({networkCount.messaging})</button>
-      <button>Notifications ({networkCount.notifications})</button>
+      <button>Messaging {networkCount.messaging}</button>
+      <button>Notifications {networkCount.notifications}</button>
 
       <button>Me ({totalNotificationCount})</button>
     </>
--- Recoil/7.4-async-queries/src/atoms.js
+++ Recoil/7.4-async-queries/src/atoms.js
@@ -1,8 +1,20 @@
+import axios from 'axios';
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
   key: "networkAtom",
-  default: {},
+  // default value cannot be async, selecter can be async
+  default: selector({
+    key: "networkAtomSelecter",
+    get: async () => {
+      
+      // Overall, the snippet creates a promise that resolves after 3 seconds, and await is used to pause the execution of the async function until the promise resolves. This effectively creates a 3-second delay.
+      await new Promise((r) => setTimeout(r, 2000)); 
+      // fetch after 3 sec
+      const res = await axios.get("https://sum-server.100xdevs.com/notifications")
+      return res.data
+    },
+  }),
 });
 
 export const totalNotificationSelector = selector({
