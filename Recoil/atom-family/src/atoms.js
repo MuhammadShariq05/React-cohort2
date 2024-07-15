31fc842f0e3b352f3aca10a6454837594419850a
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-15 11:13:33 +0530

    understood how atomfamily works

--- Recoil/atom-family/src/atoms.js
+++ Recoil/atom-family/src/atoms.js
@@ -2,8 +2,20 @@ import { atomFamily } from "recoil";
 import { TODOS } from "./todos";
 
 export const todosAtomFamily = atomFamily({
-  key: 'todosAtomFamily',
-  default: id => {
-    return TODOS.find(x => x.id === id)
+  key: "todosAtomFamily",
+  default: (id) => {
+    return TODOS.find((x) => x.id === id);
   },
 });
+
+/* For not USING ATOMFAMILY
+    alternate hard code for atomFamily
+    function atomFamily(){
+        return atom();
+    }
+
+
+    in App.jsx change the content
+    const todo = useRecoilValue(atomFamily(id));
+    
+*/ 
