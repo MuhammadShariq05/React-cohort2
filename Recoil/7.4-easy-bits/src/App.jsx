8d2c52612531629b07588e36582583f458faa97f
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-15 09:59:35 +0530

    fixes

--- Recoil/7.4-easy-bits/src/App.jsx
+++ Recoil/7.4-easy-bits/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { RecoilRoot, useRecoilValue } from "recoil";
-import { jobsAtom, networkAtom, notificationAtom, messagingAtom, totalMeCountSelecter } from "./atoms";
+import {
+  jobsAtom,
+  networkAtom,
+  notificationAtom,
+  messagingAtom,
+  totalMeCountSelecter,
+} from "./atoms";
 import { useMemo } from "react";
 
 function App() {
-  
-
   return (
     <>
       <RecoilRoot>
@@ -21,31 +25,27 @@ function App() {
 function RecoilApp() {
   const networkCount = useRecoilValue(networkAtom);
   const jobCount = useRecoilValue(jobsAtom);
-  const notificationCount = useRecoilValue(notificationAtom)
-  const messagingCount = useRecoilValue(messagingAtom)
+  const notificationCount = useRecoilValue(notificationAtom);
+  const messagingCount = useRecoilValue(messagingAtom);
 
   // Want to add the total into me
-  // first way to do it 
+  // first way to do it
   // using memo
 
   /* const totalMeCount = useMemo(()=>{
     return networkCount + jobCount + notificationCount + messagingCount;
   }, [networkCount, jobCount, notificationCount, messagingCount]) */
 
-  // somthing better than this, recoil gives us access to a 
+  // somthing better than this, recoil gives us access to a
   // component called seleter which do the same job as useMemo
   // in a recoil way
   const totalMeCount = useRecoilValue(totalMeCountSelecter);
-
 
   return (
     <div class="topnav">
       <button>Home</button>
 
-      <button>
-        My Network (
-        {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
-      </button>
+      <button>My Network ({networkCount >= 100 ? "99+" : networkCount})</button>
       <button>Jobs ( {jobCount} )</button>
       <button>Messaging ( {messagingCount} )</button>
       <button>Notification ( {notificationCount} )</button>
