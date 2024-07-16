4c25159749e4de540a4ec6e4aae524d01a440dd9
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-07-16 10:29:59 +0530

    atom selector learnt

--- Recoil/selector-family/src/App.jsx
+++ Recoil/selector-family/src/App.jsx
@@ -1,17 +1,21 @@
-
-import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
-import { todosAtomFamily } from './atoms';
+import "./App.css";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { todosAtomFamily } from "./atoms";
 
 function App() {
-  return <RecoilRoot>
-    <Todo id={1}/>
-    <Todo id={2} />
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <Todo id={1} />
+      <Todo id={2} />
+      <Todo id={3} />
+      <Todo id={4} />
+      <Todo id={5} />
+    </RecoilRoot>
+  );
 }
 
-function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+function Todo({ id }) {
+  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
 
   return (
     <>
@@ -19,7 +23,7 @@ function Todo({id}) {
       {todo.description}
       <br />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
