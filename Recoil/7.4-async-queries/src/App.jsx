a9e35f72ac84109baebb503be27f8503e4cb52ea
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-08-11 10:49:57 +0530

    recoil update

--- Recoil/7.4-async-queries/src/App.jsx
+++ Recoil/7.4-async-queries/src/App.jsx
@@ -29,12 +29,12 @@ function MainApp() {
       <button>Home</button>
 
       <button>
-        My network{" "}
-        {networkCount.networks >= 100 ? "99+" : networkCount.networks}
+        My Network
+        ({networkCount.network >= 100 ? "99+" : networkCount.network})
       </button>
-      <button>Jobs {networkCount.jobs}</button>
-      <button>Messaging {networkCount.messaging}</button>
-      <button>Notifications {networkCount.notifications}</button>
+      <button>Jobs ({networkCount.jobs})</button>
+      <button>Messaging ({networkCount.messaging})</button>
+      <button>Notifications ({networkCount.notifications})</button>
 
       <button>Me ({totalNotificationCount})</button>
     </>
--- Recoil/7.4-async-queries/src/atoms.js
+++ Recoil/7.4-async-queries/src/atoms.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
@@ -7,16 +7,18 @@ export const notifications = atom({
   default: selector({
     key: "networkAtomSelecter",
     get: async () => {
-      
       // Overall, the snippet creates a promise that resolves after 3 seconds, and await is used to pause the execution of the async function until the promise resolves. This effectively creates a 3-second delay.
-      await new Promise((r) => setTimeout(r, 2000)); 
+      await new Promise((r) => setTimeout(r, 2000));
       // fetch after 3 sec
-      const res = await axios.get("https://sum-server.100xdevs.com/notifications")
-      return res.data
+      const res = await axios.get(
+        "https://sum-server.100xdevs.com/notifications"
+      );
+      return res.data;
     },
   }),
 });
 
+// it works as usememo
 export const totalNotificationSelector = selector({
   key: "totalNotificationSelector",
   get: ({ get }) => {
